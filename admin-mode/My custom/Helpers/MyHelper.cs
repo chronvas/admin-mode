10a71e17e5917aa0d39e5ba06eec84c518d631b4
chronvas <chron.vas@outlook.com>
2015-09-10 14:11:57 +0300

    added the first functional html ajax helpers

--- admin-mode/My custom/Helpers/MyHelper.cs
+++ admin-mode/My custom/Helpers/MyHelper.cs
@@ -1,22 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
 using System.Web.Mvc.Html;
 using System.Web.Routing;
+using admin_mode.Models;
+using Microsoft.Ajax.Utilities;
 
 namespace admin_mode.My_custom.Helpers
 {
     public static class MyHelper
     {
+        public static IHtmlString LabelWithMark(this HtmlHelper helper, string content)
+        { 
+            var label = new TagBuilder("label");
+            var mark = new TagBuilder("mark");
+
+            mark.SetInnerText(content);
+
+            label.InnerHtml = mark.ToString();
+             
+            return MvcHtmlString.Create(mark.ToString());
+             
+        }
+
+        public static MvcHtmlString DisplayComboItemsIndd(this AjaxHelper ajaxHelper, ICollection<ComboItem> comboItems)
+        { 
+            var dd = new TagBuilder("dd");
+            string result = "-";
+            if (comboItems.Count == 0)
+                dd.SetInnerText(result);
+            else
+            {
+                result = "";
+                foreach (var comboItem in comboItems)
+                {
+                    dd.SetInnerText(comboItem.Name);
+                    result = result + dd;
+                }
+                return MvcHtmlString.Create(result);
+            } 
+            return MvcHtmlString.Create(dd.ToString());
+        }
+        public static MvcHtmlString DisplayStringIndd(this AjaxHelper ajaxHelper, ICollection<string> stringCollection)
+        {
+            var dd = new TagBuilder("dd");
+            string result = "-";
+            if (stringCollection.Count == 0)
+                dd.SetInnerText(result);
+            else
+            {
+                result = "";
+                foreach (var comboItem in stringCollection)
+                {
+                    dd.SetInnerText(comboItem);
+                    result = result + dd;
+                }
+                return MvcHtmlString.Create(result);
+            }
+            return MvcHtmlString.Create(dd.ToString());
+        }
+        public static IHtmlString DisplayPhoneNumber(this AjaxHelper ajaxHelper,string phonenumber)
+        {
+            var dd = "-";   
+            if (!phonenumber.IsNullOrWhiteSpace()) 
+                dd = phonenumber; 
+
+            return MvcHtmlString.Create(dd);
+        }
+
+
+        public static MvcHtmlString MactionLinkHtml(this AjaxHelper ajaxHelper, string linkText, string actionName,
+                                                    string controllerName, object routeValues, AjaxOptions ajaxOptions, 
+                                                    object htmlAttributes)
+        {
+            var repID = Guid.NewGuid().ToString();
+            var lnk = ajaxHelper.ActionLink(repID, actionName, controllerName, routeValues, ajaxOptions, htmlAttributes);
+            return MvcHtmlString.Create(lnk.ToString().Replace(repID, linkText));
+        }
+
+
         public static MvcHtmlString NoEncodeActionLink(this HtmlHelper htmlHelper,
-            string text,
-            string title,
-            string action,
-            string controller,
-            object routeValues = null,
-            object htmlAttributes = null)
+                                                       string text,
+                                                       string title,
+                                                       string action,
+                                                       string controller,
+                                                       object routeValues = null,
+                                                       object htmlAttributes = null)
         {
             //example @Html.NoEncodeActionLink("<span class='glyphicon glyphicon-plus'></span>", "the title", "UserEdit", "AdminMainPage", routeValues: new { id = @Model.Id }, htmlAttributes: new { data_modal = "", @class = "btn btn-primary" }))
             UrlHelper urlHelper = new UrlHelper(htmlHelper.ViewContext.RequestContext);
@@ -43,12 +116,8 @@ namespace admin_mode.My_custom.Helpers
             var routeData = htmlHelper.ViewContext.RouteData;
             var currentAction = routeData.GetRequiredString("action");
             var currentController = routeData.GetRequiredString("controller");
-            if (string.Equals(currentAction,
-                              action,
-                              StringComparison.OrdinalIgnoreCase) &&
-                string.Equals(currentController,
-                              controller,
-                              StringComparison.OrdinalIgnoreCase))
+            if (string.Equals(currentAction,action,StringComparison.OrdinalIgnoreCase) &&
+                    string.Equals(currentController,controller,StringComparison.OrdinalIgnoreCase))
             {
                 li.AddCssClass("active");
             }
